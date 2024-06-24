95a487dcca29113931957fa923fd0e09f9b7ccc4
abodhdh <1.abdelrhman.mo.1@gmail.com>
2024-06-24 12:04:01 +0300

    creat 1 app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+names = {111:'ali',222:'ahmed',333:'hssan'}
+def your_number(num):
+    for i in names.keys():
+        if i == num:
+            print(names[i])
+            break
+        else:
+            print('sorry')
+your_number(333)
+
